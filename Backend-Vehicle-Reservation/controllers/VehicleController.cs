ef81c595cbda514a3e09f05ff3a300eeb2a582e4
rhogger <rhoggerrv@gmail.com>
2024-04-28 00:56:40 -0300

    :sparkles: feat: Controller de Vehicle foi implementado todos as ações necessarias

--- Backend-Vehicle-Reservation/controllers/VehicleController.cs
+++ Backend-Vehicle-Reservation/controllers/VehicleController.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 using VehicleReservation.Models.Entities;
 
@@ -8,20 +9,29 @@ namespace VehicleReservation.Controllers;
 public class VehicleController : ControllerBase
 {
   private readonly ILogger<VehicleController> _logger;
-  private readonly List<Vehicle> vehicles = [];
-
-  public VehicleController(ILogger<VehicleController> logger)
+  private readonly IVehicleService _vehicleService;
+  public VehicleController(ILogger<VehicleController> logger, IVehicleService vehicleService)
   {
     _logger = logger;
+    _vehicleService = vehicleService;
+  }
+
+  [HttpGet(Name = "Get vehicles by all filters")]
+  public IActionResult GetByFilter([FromQuery] string? year, string? make, int? passengerCapacity)
+  {
+    var vehicles = _vehicleService.GetByFilter(year, make, passengerCapacity);
+
+    if (vehicles.Any()) return Ok(vehicles);
+    else return NotFound("No vehicles found for the specified filter.");
   }
 
   [HttpPost(Name = "Create vehicle")]
   public IActionResult Create([FromBody] Vehicle vehicle)
   {
-    vehicle.Id = vehicles.Count + 1;
-    vehicles.Add(vehicle);
+    vehicle.Id = Guid.NewGuid().ToString();
+    _vehicleService.Add(vehicle);
 
-    return CreatedAtAction(nameof(Create), new { id = vehicle.Id }, "Vehicle created at successfully");
+    return CreatedAtAction(nameof(Create), new { id = vehicle.Id }, vehicle);
   }
 }
 
