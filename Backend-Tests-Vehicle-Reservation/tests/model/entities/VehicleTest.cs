70385ee847799396037785a1715e335e702f6fa9
rhogger <rhoggerrv@gmail.com>
2024-05-08 00:04:37 -0300

    :white_check_mark: test: Adaptando os testes as mudanças das classes e criando mais testes e casos

--- Backend-Tests-Vehicle-Reservation/tests/model/entities/VehicleTest.cs
+++ Backend-Tests-Vehicle-Reservation/tests/model/entities/VehicleTest.cs
@@ -8,7 +8,7 @@ public class VehicleTest
   [InlineData("")]
   [InlineData(null)]
   [InlineData("Nissan")]
-  public void Constructor_NullOrEmptyMake_ThrowsException(string _make)
+  public void IsValidMakeValue(string _make)
   {
     // Arrange
     string make = _make;
@@ -18,19 +18,19 @@ public class VehicleTest
     string plate = "NGA0881";
     int passengerCapacity = 5;
 
-    // Act & Assert
-    try
-    {
-      Vehicle vehicle = new Vehicle(make, model, year, color, plate, passengerCapacity);
+    Vehicle vehicle = new Vehicle(make, model, year, color, plate, passengerCapacity);
 
-      // Assert
-      Assert.NotNull(vehicle);
-      Assert.Equal(make, vehicle.make);
-    }
-    catch (ArgumentNullException)
+    // Act
+    bool isValidMake = vehicle.IsValidMake();
+
+    // Assert
+    if (!string.IsNullOrEmpty(make))
     {
-      // Assert
-      Assert.True(string.IsNullOrEmpty(make));
+      Assert.True(isValidMake);
+    }
+    else
+    {
+      Assert.False(isValidMake);
     }
   }
 
@@ -38,7 +38,7 @@ public class VehicleTest
   [InlineData("")]
   [InlineData(null)]
   [InlineData("Skyline GT-R")]
-  public void Constructor_NullOrEmptyModel_ThrowsException(string _model)
+  public void IsValidModelValue(string _model)
   {
     // Arrange
     string make = "Nissan";
@@ -48,19 +48,19 @@ public class VehicleTest
     string plate = "NGA0881";
     int passengerCapacity = 5;
 
-    // Act & Assert
-    try
-    {
-      Vehicle vehicle = new Vehicle(make, model, year, color, plate, passengerCapacity);
+    Vehicle vehicle = new Vehicle(make, model, year, color, plate, passengerCapacity);
 
-      // Assert
-      Assert.NotNull(vehicle);
-      Assert.Equal(model, vehicle.model);
-    }
-    catch (ArgumentNullException)
+    // Act
+    bool isValidModel = vehicle.IsValidModel();
+
+    // Assert
+    if (!string.IsNullOrEmpty(model))
     {
-      // Assert
-      Assert.True(string.IsNullOrEmpty(model));
+      Assert.True(isValidModel);
+    }
+    else
+    {
+      Assert.False(isValidModel);
     }
   }
 
@@ -68,7 +68,7 @@ public class VehicleTest
   [InlineData("")]
   [InlineData(null)]
   [InlineData("2024")]
-  public void Constructor_NullOrEmptyYear_ThrowsException(string _year)
+  public void IsValidYearValue(string _year)
   {
     // Arrange
     string make = "Nissan";
@@ -78,70 +78,109 @@ public class VehicleTest
     string plate = "NGA0881";
     int passengerCapacity = 5;
 
-    // Act & Assert
-    try
-    {
-      Vehicle vehicle = new Vehicle(make, model, year, color, plate, passengerCapacity);
+    Vehicle vehicle = new Vehicle(make, model, year, color, plate, passengerCapacity);
 
-      // Assert
-      Assert.NotNull(vehicle);
-      Assert.Equal(year, vehicle.year);
-    }
-    catch (ArgumentNullException)
+    // Act
+    bool isValidYear = vehicle.IsValidYear();
+
+    // Assert
+    if (!string.IsNullOrEmpty(year))
     {
-      // Assert
-      Assert.True(string.IsNullOrEmpty(year));
+      Assert.True(isValidYear);
+    }
+    else
+    {
+      Assert.False(isValidYear);
     }
   }
 
-  // [Theory]
-  // [InlineData("")]
-  // [InlineData(null)]
-  // public void Constructor_NullOrEmptyColor_ThrowsException()
-  // {
-  //   // Arrange
-  //   string make = "Make";
-  //   string model = "Model";
-  //   string year = "2024";
-  //   string color = "";
-  //   string plate = "Plate";
-  //   int passengerCapacity = 5;
+  [Theory]
+  [InlineData("")]
+  [InlineData(null)]
+  [InlineData("Azul")]
+  public void IsValidColorValue(string _color)
+  {
+    // Arrange
+    string make = "Nissan";
+    string model = "Skyline GT-R";
+    string year = "2024";
+    string color = _color;
+    string plate = "NGA0881";
+    int passengerCapacity = 5;
 
-  //   // Act & Assert
-  //   Assert.Throws<ArgumentNullException>(() => new Vehicle(make, model, year, color, plate, passengerCapacity));
-  // }
+    Vehicle vehicle = new Vehicle(make, model, year, color, plate, passengerCapacity);
 
-  // [Theory]
-  // [InlineData("")]
-  // [InlineData(null)]
-  // public void Constructor_NullOrEmptyPlate_ThrowsException()
-  // {
-  //   // Arrange
-  //   string make = "Make";
-  //   string model = "Model";
-  //   string year = "2024";
-  //   string color = "Color";
-  //   string plate = "";
-  //   int passengerCapacity = 5;
+    // Act
+    bool isValidColor = vehicle.IsValidColor();
 
-  //   // Act & Assert
-  //   Assert.Throws<ArgumentNullException>(() => new Vehicle(make, model, year, color, plate, passengerCapacity));
-  // }
+    // Assert
+    if (!string.IsNullOrEmpty(color))
+    {
+      Assert.True(isValidColor);
+    }
+    else
+    {
+      Assert.False(isValidColor);
+    }
+  }
 
-  // [Theory]
-  // [InlineData("")]
-  // [InlineData(null)]
-  // public void Constructor_PassengerCapacityLessThanOne_ThrowsException()
-  // {
-  //   // Arrange
-  //   string make = "Make";
-  //   string model = "Model";
-  //   string year = "2024";
-  //   string color = "Color";
-  //   string plate = "Plate";
-  //   int passengerCapacity = 0;
+  [Theory]
+  [InlineData("")]
+  [InlineData(null)]
+  [InlineData("NGA0881")]
+  public void IsValidPlateValue(string _plate)
+  {
+    // Arrange
+    string make = "Nissan";
+    string model = "Skyline GT-R";
+    string year = "2024";
+    string color = "Azul";
+    string plate = _plate;
+    int passengerCapacity = 5;
 
-  //   // Act & Assert
-  //   Assert.Throws<Exception>(() => new Vehicle(make, model, year, color, plate, passengerCapacity));
-  // }
+    Vehicle vehicle = new Vehicle(make, model, year, color, plate, passengerCapacity);
+
+    // Act
+    bool isValidPlate = vehicle.IsValidPlate();
+
+    // Assert
+    if (!string.IsNullOrEmpty(plate))
+    {
+      Assert.True(isValidPlate);
+    }
+    else
+    {
+      Assert.False(isValidPlate);
+    }
+  }
+
+  [Theory]
+  [InlineData(0)]
+  [InlineData(1)]
+  [InlineData(5)]
+  public void IsValidNumberOfPassengersValue(int _passengersCapacity)
+  {
+    // Arrange
+    string make = "Nissan";
+    string model = "Skyline GT-R";
+    string year = "2024";
+    string color = "Azul";
+    string plate = "NGA0881";
+    int passengerCapacity = _passengersCapacity;
+
+    Vehicle vehicle = new Vehicle(make, model, year, color, plate, passengerCapacity);
+
+    // Act
+    bool isValidNumberOfPassengers = vehicle.IsValidNumberOfPassengers();
+
+    // Assert
+    if (passengerCapacity < 1)
+    {
+      Assert.False(isValidNumberOfPassengers);
+    }
+    else
+    {
+      Assert.True(isValidNumberOfPassengers);
+    }
+  }
 }
