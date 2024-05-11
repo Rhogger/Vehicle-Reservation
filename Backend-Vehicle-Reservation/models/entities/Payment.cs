8dd2248d49fb65039dda77ef9506855b413f59f3
rhogger <rhoggerrv@gmail.com>
2024-05-11 17:06:38 -0300

    :construction: cd: Adicioção de validações no Payment (Model)

--- Backend-Vehicle-Reservation/models/entities/Payment.cs
+++ Backend-Vehicle-Reservation/models/entities/Payment.cs
@@ -16,6 +16,55 @@ public class Payment
   {
     this.reservation_id = reservation_id;
     this.value = value;
-    this.type = type;
+    this.type = type.ToLower();
+  }
+
+  public bool IsValidReservationId()
+  {
+    bool valid = true;
+
+    try
+    {
+      if (reservation_id < 0) throw new Exception("The reservation ID is invalid.");
+    }
+    catch
+    {
+      valid = false;
+    }
+
+    return valid;
+  }
+
+  public bool IsValidValue()
+  {
+    bool valid = true;
+
+    try
+    {
+      if (value <= 0) throw new Exception("Value of payment is 0 or less.");
+    }
+    catch
+    {
+      valid = false;
+    }
+
+    return valid;
+  }
+
+  public bool IsValidType()
+  {
+    bool valid = true;
+
+    try
+    {
+      if (string.IsNullOrEmpty(type)) throw new ArgumentNullException("Type of payment can't be null or empty string.");
+      if (type != "boleto" || type != "cartao_credito" || type != "cartao_debito") throw new ArgumentOutOfRangeException("Payment way is invalid.");
+    }
+    catch
+    {
+      valid = false;
+    }
+
+    return valid;
   }
 }
